3960c8064b3766752af8ca4e6926f12b9de87ddf
Chomusuke <yukhanov.artur@gmail.com>
2024-11-04 16:19:48 +0100

    Implemented first connection to the server

--- code/IlianTopalov_chomusuke-works/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/IlianTopalov_chomusuke-works/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,13 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
-	final String SERVER_ADDRESS = "1.2.3.4";
-	final int SERVER_PORT = 1234;
+	final String SERVER_ADDRESS = "10.193.24.163";
+	final int SERVER_PORT = 25565;
 
 	public static void main(String[] args) {
 		// Create a new client and run it
@@ -11,5 +16,25 @@ public class Client {
 	}
 
 	private void run() {
+		try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+			var is = new BufferedReader(new InputStreamReader(
+				socket.getInputStream(),
+				StandardCharsets.UTF_8
+			));
+
+			var os = new BufferedWriter(new OutputStreamWriter(
+				socket.getOutputStream(),
+				StandardCharsets.UTF_8
+			));
+
+			String serverResponse;
+
+			while ((serverResponse = is.readLine()) != null) {
+				System.out.println(serverResponse);
+			}
+		} catch (IOException e) {
+			System.err.println("Exception was handled");
+			System.err.println(e.getMessage());
+		}
 	}
 }
