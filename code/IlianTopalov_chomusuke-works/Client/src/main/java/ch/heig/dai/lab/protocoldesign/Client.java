171909d34e6e0d51419ee78b8bdfc77a8b6b0212
Chomusuke <yukhanov.artur@gmail.com>
2024-11-07 22:24:20 +0100

    Partial implementation of the client, more testing needed

--- code/IlianTopalov_chomusuke-works/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/IlianTopalov_chomusuke-works/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -3,10 +3,14 @@ package ch.heig.dai.lab.protocoldesign;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 	final String SERVER_ADDRESS = "localhost";
 	final int SERVER_PORT = 25565;
+
+	final String EXIT_CODE = "BYE";
 
 	public static void main(String[] args) {
 		// Create a new client and run it
@@ -26,10 +30,21 @@ public class Client {
 				StandardCharsets.UTF_8
 			));
 
-			String serverResponse;
+			int lineCount = Integer.parseInt(is.readLine());
 
-			while ((serverResponse = is.readLine()) != null) {
-				System.out.println(serverResponse);
+			for (int i = 0; i < lineCount; ++i) {
+				System.out.printf("%s\n", is.readLine());
+			}
+
+			Scanner scanner = new Scanner(System.in);
+
+			String input;
+
+			while (!(input = scanner.nextLine()).equals(EXIT_CODE)) {
+				os.write(input + '\n');
+				os.flush();
+
+				System.out.println(is.readLine());
 			}
 
 			is.close();
